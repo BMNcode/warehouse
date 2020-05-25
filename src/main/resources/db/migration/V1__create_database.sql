607207aff05f30dedf3bf33d4bab93a799371822
Максим <happymax@list.ru>
2020-05-25 22:36:35 +0300

    the establishment of a training project warehouse. stack: maven, flyway, postgres

---
+++ src/main/resources/db/migration/V1__create_database.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS Public.goods (
+  id_product int8 not null,
+  name_product varchar(255) not null,
+  count_product int8,
+  unit_product varchar(20),
+  description varchar(1000),
+  PRIMARY KEY (id_product)
+) WITHOUT OIDS;
+
+CREATE TABLE IF NOT EXISTS Public.warehouse (
+  id_product int8 not null references goods(id_product),
+  date_receipt timestamp not null,
+  count_product int8 not null,
+  description varchar(1000)
+) WITHOUT OIDS;
+
+CREATE TABLE IF NOT EXISTS Public.orders (
+  id_order int8 not null,
+  id_product int8 not null references goods(id_product),
+  organization varchar(255) not null,
+  count_order int8 not null,
+  description varchar(1000),
+  PRIMARY KEY (id_order)
+) WITHOUT OIDS;
+
+CREATE TABLE IF NOT EXISTS Public.release_goods (
+  id_order int8 not null references orders(id_order),
+  id_product int8 not null references goods(id_product),
+  date_release timestamp not null,
+  count_product int8 not null,
+  description varchar(1000)
+) WITHOUT OIDS;
